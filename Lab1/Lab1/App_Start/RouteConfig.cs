69b805defb7f2b471f9341af6d6f25ec2749e36f
Shane Egan <shane.egan93@gmail.com>
2015-09-26 20:01:47 -0400

    Initial commit

---
+++ Lab1/Lab1/App_Start/RouteConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Lab1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: null,
+                url: "{controller}/{action}/{id}",
+                defaults: new { 
+                    controller = "Pages", 
+                    action = "Step1", 
+                    id = UrlParameter.Optional
+                }
+            );
+
+            routes.MapRoute(
+                name: null,
+                url: "{controller}/{action}/{id}",
+                defaults: new
+                {
+                    controller = "Pages",
+                    action = "Step2",
+                    id = UrlParameter.Optional
+                }
+            );
+
+            routes.MapRoute(
+                name: null,
+                url: "{controller}/{action}/{id}",
+                defaults: new
+                {
+                    controller = "Pages",
+                    action = "Step3",
+                    id = UrlParameter.Optional
+                }
+            );
+        }
+    }
+}
---
+++ Lab1/Lab1/Controllers/PagesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class PagesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Step1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Step1(FormCollection formCollection)
+        {
+            Session["username"] = formCollection["username"];
+
+            return RedirectToAction("Step2");
+        }
+
+        [HttpGet]
+        public ActionResult Step2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Step2(FormCollection formCollection)
+        {
+            Session["favColour"] = formCollection["favColour"];
+
+            return RedirectToAction("Step3");
+        }
+
+        [HttpGet]
+        public ActionResult Step3()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Step3(FormCollection formCollection)
+        {
+            formCollection = null;
+            Session.Clear();
+            return View();
+        }
+    }
+}
