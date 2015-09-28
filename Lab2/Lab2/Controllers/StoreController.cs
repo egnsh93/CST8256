6fd3ae78636d50ddb99d27228822818a1020ab31
Shane Egan <shane.egan93@gmail.com>
2015-09-28 00:25:01 -0400

    Add Controller, Models and Repository

---
+++ Lab2/Lab2/Controllers/StoreController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab2.Repositories;
+using Lab2.ViewModels;
+
+namespace Lab2.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            // Get book data from repo
+            var bookRepository = BookRepository.GetAllBookData();
+
+            // Instantiate the viewmodel
+            var bookViewModel = new BookViewModel
+            {
+                BookData = bookRepository
+            };
+
+            // Send viewmodel into the view
+            return View("Index", bookViewModel);
+        }
+
+        // POST: Store
+        [HttpPost]
+        public ActionResult Index(BookViewModel bookViewModel)
+        {
+            // Get book data from repo based on selected book in the view
+            var book = BookRepository.GetBookById(bookViewModel.Id);
+
+            // TODO: Get selected ID from view and display book info
+
+            // Send viewmodel into the view
+            return View(bookViewModel);
+        }
+
+        // GET: Cart
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab2/Lab2/ViewModels/BookViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lab2.Models;
+
+namespace Lab2.ViewModels
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int SelectedValue { get; set; }
+        public virtual IEnumerable<Book> BookData { get; set; } 
+    }
+}
