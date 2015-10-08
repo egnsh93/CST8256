49ecbbb00ebf43f47889406493f983c8f6fce10f
Shane Egan <shane.egan93@gmail.com>
2015-10-07 20:51:46 -0400

    Update models

---
+++ Lab2/Lab2/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lab2.Repositories;
+
+namespace Lab2.Models
+{
+    public class Cart
+    {
+        public List<Order> BookOrders { get; set; }
+    }
+}
---
+++ Lab2/Lab2/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab2.Models
+{
+    public class Order
+    {
+        public Book Book { get; private set; }
+        public int NumCopies { get; set; }
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public double SubTotal
+        {
+            get { return Book.Price*NumCopies; }
+        }
+
+        public Order(Book book, int numCopies)
+        {
+            Book = book;
+            NumCopies = numCopies;
+        }
+    }
+}
