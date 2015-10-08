5e5a9618a4c4588c96af57d0b6e8212201fbf8c9
Shane Egan <shane.egan93@gmail.com>
2015-10-07 20:51:32 -0400

    Add repository class to handle user orders

---
+++ Lab2/Lab2/Repositories/OrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lab2.Models;
+
+namespace Lab2.Repositories
+{
+    public class OrderRepository
+    {
+        public static List<Order> OrderList = new List<Order>();
+
+        public static void SaveOrder(Order order)
+        {
+            OrderList.Add(order);
+        }
+
+        public static List<Order> RetrieveAllOrders()
+        {
+            return OrderList;
+        }
+
+        public static void RemoveAllorders()
+        {
+            OrderList.Clear();
+        }
+    }
+}
