779c900912366eb505f4dd4bdb316538d6b34cac
Shane Egan <shane.egan93@gmail.com>
2015-10-26 18:29:01 -0400

    Add model state validation

--- Lab4/Lab4/Controllers/CourseController.cs
+++ Lab4/Lab4/Controllers/CourseController.cs
@@ -1,13 +1,27 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using Lab4.Models;
+using Lab4.ViewModels;
 
 namespace Lab4.Controllers
 {
     public class CourseController : Controller
     {
         // GET: Course
-        public ActionResult Index()
+        public ActionResult Add() => View("AddCourse");
+
+        [HttpPost]
+        public ActionResult Add(CourseViewModel input)
         {
-            return View();
+            // Validate the model
+            if (ModelState.IsValid == false) return View("AddCourse");
+
+            // If everything is OK, create the course and store in application state
+            HttpContext.Application.Lock();
+            HttpContext.Application["course"] = new Course(input.Number, input.Name, Convert.ToInt16(input.Hours));
+            HttpContext.Application.UnLock();
+
+            return RedirectToAction("Add", "Student");
         }
     }
 }
