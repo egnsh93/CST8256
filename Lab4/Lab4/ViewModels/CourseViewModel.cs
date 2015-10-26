c8f1ec5927ad6892b856753988b438f12c3435d1
Shane Egan <shane.egan93@gmail.com>
2015-10-26 18:29:20 -0400

    Add ViewModels

---
+++ Lab4/Lab4/ViewModels/CourseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Lab4.Models;
+
+namespace Lab4.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required(ErrorMessage = "Required")]
+        public string Number { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public int Hours { get; set; }
+
+        public List<Student> Students { get; set; } 
+    }
+}
---
+++ Lab4/Lab4/ViewModels/StudentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab4.ViewModels
+{
+    public class StudentViewModel
+    {
+        [Required(ErrorMessage = "Required")]
+        public int Number { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+}
