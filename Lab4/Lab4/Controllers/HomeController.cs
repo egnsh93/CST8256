309fab4825cba2798f7e7b65d920756d5dec0e4f
Shane Egan <shane.egan93@gmail.com>
2015-10-26 18:27:03 -0400

    Update routing logic

--- Lab4/Lab4/Controllers/HomeController.cs
+++ Lab4/Lab4/Controllers/HomeController.cs
@@ -6,7 +6,20 @@ namespace Lab4.Controllers
     {
         public ActionResult Index()
         {
-            return RedirectToAction("Add", "User");
+            // If user account does not exist, redirect to add user form
+            if ((string) HttpContext.Application["name"] == null)
+            {
+                return RedirectToAction("Add", "User"); 
+            }
+
+            // If user exists, but course does not, redirect to add course form
+            if ((string)HttpContext.Application["name"] != null && HttpContext.Application["course"] == null)
+            {
+                return RedirectToAction("Add", "Course");
+            }
+
+            // If a user and a course already exist, redirect to add student form
+            return RedirectToAction("Add", "Student");
         }
     }
 }
