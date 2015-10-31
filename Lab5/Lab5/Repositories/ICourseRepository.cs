c728b5071f492afb0df9b91008ca1cd99451d0f7
Shane Egan <shane.egan93@gmail.com>
2015-10-31 15:52:12 -0400

    Add course repository interface

---
+++ Lab5/Lab5/Repositories/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Lab5.Models;
+
+namespace Lab5.Repositories
+{
+    public interface ICourseRepository
+    {
+        void InsertCourse(Course course);
+        Course GetCourse(string id);
+        List<Course> GetCourses();
+    }
+}
