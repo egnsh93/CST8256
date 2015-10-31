93363a5a389cd480678c34cf8f74b59f1746abdd
Shane Egan <shane.egan93@gmail.com>
2015-10-31 15:54:24 -0400

    Major refactor. Add dependency injection and repository pattern

--- Lab5/Lab5/Controllers/CourseController.cs
+++ Lab5/Lab5/Controllers/CourseController.cs
@@ -1,16 +1,36 @@
 ﻿using System.Data.SqlClient;
 using System.Web.Configuration;
 using System.Web.Mvc;
+using Lab5.Models;
+using Lab5.Repositories;
 using Lab5.ViewModels;
+using Ninject;
+using Ninject.Extensions.ContextPreservation;
 
 namespace Lab5.Controllers
 {
     public class CourseController : Controller
     {
+        private readonly ICourseRepository _repository;
+
+        public CourseController(ICourseRepository repository)
+        {
+            _repository = repository;
+        }
+
         // GET: Course
         public ActionResult Add()
         {
-            return View();
+            // Get all records from the Course table
+            var courses = _repository.GetCourses();
+
+            // Send the list of courses to the view model
+            var courseViewModel = new CourseViewModel()
+            {
+                RegisteredCourses = courses,
+            };
+           
+            return View(courseViewModel);
         }
 
         [HttpPost]
@@ -18,39 +38,12 @@ namespace Lab5.Controllers
         {
             // Validate model state
             if (!ModelState.IsValid) return View(input);
+            
+            // Insert course into database via repository
+            var course = new Course(input.Number, input.Name, input.WeeklyHours);
+            _repository.InsertCourse(course);
 
-            // Define ADO.NET objects
-            var insertCourseSql = "INSERT INTO Course (CourseID, CourseTitle, HoursPerWeek) VALUES (@courseID, @courseTitle, @courseHours)";
-
-            // Define connection objects
-            var connectionString = WebConfigurationManager.ConnectionStrings["StudentRegistration"].ConnectionString;
-            var connection = new SqlConnection(connectionString);
-        
-            var sqlCommand = new SqlCommand(insertCourseSql, connection);
-
-            // Add course parameters
-            sqlCommand.Parameters.AddWithValue("@courseID", input.Number);
-            sqlCommand.Parameters.AddWithValue("@courseTitle", input.Name);
-            sqlCommand.Parameters.AddWithValue("@courseHours", input.WeeklyHours);
-
-            // Check for connection string
-            if (connectionString == null) return RedirectToAction("Add", input);
-
-            try
-            {
-                connection.Open();
-                var added = sqlCommand.ExecuteNonQuery();
-            }
-            catch
-            {
-
-            }
-            finally
-            {
-                connection.Close();
-            }
-
-            return RedirectToAction("Add", input);
+            return RedirectToAction("Add");
         }
     }
 }
