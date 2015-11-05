eac12e7a7fcddec03d730fe08b66b0f6d6654c9c
Shane Egan <shane.egan93@gmail.com>
2015-11-05 00:49:33 -0500

    Add Student view model and views

--- Lab5/Lab5/ViewModels/StudentViewModel.cs
+++ Lab5/Lab5/ViewModels/StudentViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,15 +10,17 @@ namespace Lab5.ViewModels
 {
     public class StudentViewModel
     {
-        [Required(ErrorMessage = "Required")]
-        public List<CourseOffering> RegisteredCourseOfferings { get; set; }
+        [Required(ErrorMessage = "Required"), DisplayName("Course Offering")]
+        public string SelectedCourseId { get; set; }
 
-        [Range(1, int.MaxValue), Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Required"), DisplayName("Student Number")]
         public int Number { get; set; }
 
-        [Required(ErrorMessage = "Required")]
+        [Required(ErrorMessage = "Required"), DisplayName("Student Name")]
         public string Name { get; set; }
 
         public string Type { get; set; }
+
+        public List<CourseOffering> CourseOfferings { get; set; }
     }
 }
