df48c7d0c833d8c1bb06438adb6af342b85d3ab9
Shane Egan <shane.egan93@gmail.com>
2015-11-15 20:43:42 -0500

    Move inline scripts to external file

---
+++ Lab5/Lab5/Scripts/site.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $("#SelectedCourseId").change(function () {
+        var $opt = $(this.options[this.selectedIndex]),
+            id = $(this).val(),
+            year = $opt.attr("data-year"),
+            semester = $opt.attr("data-semester");
+
+        $.ajax({
+            type: "GET",
+            url: "/Student/GetStudentsInOffering",
+            data: { offeringId: id, year: year, semester: semester },
+            dataType: "HTML",
+            cache: false,
+            success: function (result) {
+                $("#studentsInOffering").html(result);
+            },
+            error: function (xhr, status, error) {
+                var err = eval("(" + xhr.responseText + ")");
+                alert(err.Message);
+            }
+        });
+    });
+});
