dc697435316ae8dc71476743f91d1197010b96ad
Shane Egan <shane.egan93@gmail.com>
2015-11-20 11:21:28 -0500

    Add partial models

---
+++ Lab6/Lab6/Models/CourseOffering.partial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab6.Models
+{
+    public partial class CourseOffering
+    {
+        public Course CourseOffered { get; set; }
+
+        public int CompareTo(CourseOffering other) => other == null ? 1 : Year.CompareTo(other.Year);
+
+        public override string ToString()
+        {
+            return Course_CourseID + " " + Course.CourseTitle + " " + Year + " " + Semester;
+        }
+    }
+}
