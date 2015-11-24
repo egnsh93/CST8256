d71c63e40a6a5372727788742a7e751aa05a7fab
Shane Egan <shane.egan93@gmail.com>
2015-11-23 19:40:38 -0500

    Add Json and update the way students are retrieved

--- Lab6/Lab6/Controllers/StudentController.cs
+++ Lab6/Lab6/Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using Lab6.Exceptions;
 using Lab6.Factory;
 using Lab6.Models;
 using Lab6.Services;
@@ -31,10 +32,41 @@ namespace Lab6.Controllers
         // GET: Student
         public ActionResult Add() => View(PopulateViewModel(new StudentViewModel()));
 
-        public PartialViewResult GetStudentsInOffering(string offeringId, int year, string semester)
+        [HttpPost]
+        public JsonResult Add(StudentViewModel viewModel)
+        {
+            // Populate VM
+            viewModel = PopulateViewModel(viewModel);
+
+            // Validate model state
+            if (!ModelState.IsValid)
+                return Json(new { error = true, message = "There were errors in the submission" });
+
+            // Attempt to add the student to the database/course offering
+            try
+            {
+                // Get the created student from the factory
+                var studentFactory = new StudentFactory();
+                var student = studentFactory.CreateStudent(viewModel.Number.ToString(), viewModel.Name, viewModel.Type);
+
+                // Get the course offering
+                var offering = _courseService.GetCourseOffering(viewModel.SelectedYear, viewModel.SelectedSemester, viewModel.SelectedOffering);
+
+                // Add the student
+                _courseService.AddStudent(student, offering);
+            }
+            catch (StudentExistsException)
+            {
+                return Json(new { error = true, message = "Student already in course offering" });
+            }
+
+            return Json(new { error = false, message = "Student successfully added to course offering!" });
+        }
+
+        public PartialViewResult List(string courseId, string semester, int year)
         {
             // Get the selected course offering
-            var offering = _courseService.GetCourseOffering(year, semester, offeringId);
+            var offering = _courseService.GetCourseOffering(year, semester, courseId);
 
             // Get students in offering and sort based out custom comparer
             var students = _courseService.GetStudents(offering);
@@ -47,36 +79,6 @@ namespace Lab6.Controllers
             };
 
             return PartialView("_DisplayStudents", viewModel);
-        }
-
-        [HttpPost]
-        public ActionResult Add(StudentViewModel input)
-        {
-            // Populate VM
-            var viewModel = PopulateViewModel(input);
-
-            // Validate model state
-            if (!ModelState.IsValid) return View(input);
-
-            // Attempt to add the student to the database/course offering
-            try
-            {
-                // Get the created student from the factory
-                var studentFactory = new StudentFactory();
-                var student = studentFactory.CreateStudent(input.Number.ToString(), input.Name, input.Type);
-
-                // Get the course offering
-                var offering = _courseService.GetCourseOffering(input.SelectedYear, input.SelectedSemester, input.SelectedCourseId);
-
-                // Add the student
-                _courseService.AddStudent(student, offering);
-            }
-            catch (Exception ex)
-            {
-                TempData["Error"] = ex.ToString();
-            }
-
-            return RedirectToAction("Add", viewModel);
         }
     }
 }
