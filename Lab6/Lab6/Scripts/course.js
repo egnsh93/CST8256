c369fef1533aac40c387f8e49024213691c5831f
Shane Egan <shane.egan93@gmail.com>
2015-11-25 14:28:20 -0500

    Move JS to page specific file

---
+++ Lab6/Lab6/Scripts/course.js
@@ -0,0 +1,60 @@
+﻿$(function () {
+
+    // Get the registered courses on load
+    $.get("/Course/List")
+        .success(function (result) {
+            // Load offerings into HTML
+            $("#registeredCourses").html(result);
+        });
+
+    // When the course form is submitted
+    $("#submitCourse").submit(function (e) {
+
+        e.preventDefault();
+
+        // If valid according to unobtrusive JS
+        if ($(this).valid()) {
+
+            // Properties to send to controller
+            var properties = {
+                Number: $("#courseNumber").val(),
+                Name: $("#courseName").val(),
+                WeeklyHours: $("#courseHours").val(),
+                Description: $("#Description").val()
+            };
+
+            // Request to post the selected offering
+            $.post($(this).url, properties, function (data) {
+
+                // Determine which alert type to display
+                if (data.error === true) {
+                    $("#resultNotification").removeClass("alert-success");
+                    $("#resultNotification").addClass("alert-danger");
+                } else {
+                    $("#resultNotification").removeClass("alert-danger");
+                    $("#resultNotification").addClass("alert-success");
+                }
+
+            })
+                .success(function (data) {
+
+                    $("#resultNotification").show().text(data.message).delay(4000);
+                    $("#resultNotification").fadeOut("slow");
+
+                    // Clear form fields
+                    $("#courseNumber").val("");
+                    $("#courseName").val("");
+                    $("#courseHours").val("");
+                    $("#Description").val("");
+
+                    // Reload the registered courses
+                    $.get("/Course/List")
+                        .success(function (result) {
+                            // Load offerings into HTML
+                            $("#registeredCourses").html(result);
+                        });
+                });
+        }
+    });
+});
+
---
+++ Lab6/Lab6/Scripts/student.js
@@ -0,0 +1,82 @@
+﻿$(function () {
+
+    // Student registration page on dropdown change
+    $("#SelectedOffering").change(function (e) {
+
+        // If valid according to unobtrusive JS
+        if ($(this).valid()) {
+
+            // Properties to send to controller
+            var $opt = $(this.options[this.selectedIndex]);
+
+            var properties = {
+                SelectedOffering: $(this).val(),
+                SelectedYear: $opt.attr("data-year"),
+                SelectedSemester: $opt.attr("data-semester"),
+                Number: $("#studentNumber").val(),
+                Name: $("#studentName").val(),
+                Type: $("input[type='radio'][name='Type']:checked")
+            };
+
+            // If valid according to unobtrusive JS
+            if ($(this).valid()) {
+                $.ajax({
+                    type: $(this).action,
+                    url: "/Student/List",
+                    data: { courseId: properties.SelectedOffering, semester: properties.SelectedSemester, year: properties.SelectedYear },
+                    dataType: "HTML",
+                    cache: false,
+                    success: function (data) {
+                        $("#studentsInOffering").html(data);
+                    }
+                });
+            }
+        }
+    });
+
+    // Student registration page on submit
+    $("#submitStudent").submit(function (e) {
+
+        e.preventDefault();
+
+        // If valid according to unobtrusive JS
+        if ($(this).valid()) {
+
+            var properties = {
+                SelectedOffering: $("#SelectedOffering").val(),
+                SelectedYear: $("#SelectedOffering option:selected").attr("data-year"),
+                SelectedSemester: $("#SelectedOffering option:selected").attr("data-semester"),
+                Number: $("#studentNumber").val(),
+                Name: $("#studentName").val(),
+                Type: $("input[name=Type]:checked").val()
+            };
+
+            // Request to post the selected offering
+            $.post($(this).url, properties, function (data) {
+
+                // Determine which alert type to display
+                if (data.error === true) {
+                    $("#resultNotification").removeClass("alert-success");
+                    $("#resultNotification").addClass("alert-danger");
+                } else {
+                    $("#resultNotification").removeClass("alert-danger");
+                    $("#resultNotification").addClass("alert-success");
+                }
+
+            })
+                .success(function (data) {
+
+                    $("#resultNotification").show().text(data.message).delay(4000);
+                    $("#resultNotification").fadeOut("slow");
+
+                    // Get the students by offering
+                    $.get("/Student/List", { courseId: properties.SelectedOffering, semester: properties.SelectedSemester, year: properties.SelectedYear })
+                        .success(function (result) {
+                            // Load offerings into HTML
+                            $("#studentsInOffering").html(result);
+                        });
+                });
+        }
+    });
+});
+
